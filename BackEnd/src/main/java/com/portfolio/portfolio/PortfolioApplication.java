f1a0277a862391ebdf95db24e6ef71140f500a78
Tomás Repetto <tomyrep04@gmail.com>
2023-04-07 20:14:07 -0300

    conexion a base de datos

--- BackEnd/src/main/java/com/portfolio/portfolio/PortfolioApplication.java
+++ BackEnd/src/main/java/com/portfolio/portfolio/PortfolioApplication.java
@@ -1,13 +1,13 @@
-package com.portfolio.tomas;
+package com.portfolio.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TomasApplication {
+public class PortfolioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TomasApplication.class, args);
+		SpringApplication.run(PortfolioApplication.class, args);
 	}
 
 }
--- BackEnd/src/test/java/com/portfolio/portfolio/PortfolioApplicationTests.java
+++ BackEnd/src/test/java/com/portfolio/portfolio/PortfolioApplicationTests.java
@@ -1,10 +1,10 @@
-package com.portfolio.tomas;
+package com.portfolio.portfolio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TomasApplicationTests {
+class PortfolioApplicationTests {
 
 	@Test
 	void contextLoads() {
