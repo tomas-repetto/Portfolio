1118d010373abfdbec2a9f898081d57c06006ba6
Tomás Repetto <tomyrep04@gmail.com>
2023-04-09 19:33:46 -0300

    Delete Solucionado

--- BackEnd/src/main/java/com/portfolio/portfolio/Controller/CExperiencia.java
+++ BackEnd/src/main/java/com/portfolio/portfolio/Controller/CExperiencia.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -65,6 +66,7 @@ public class CExperiencia {
         return new ResponseEntity(new Mensaje ("Experiencia actualizada"),HttpStatus.OK);
     }
     
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         if(!sExperiencia.existsById(id))
             return new ResponseEntity(new Mensaje("El Id no existe"),HttpStatus.BAD_REQUEST);
