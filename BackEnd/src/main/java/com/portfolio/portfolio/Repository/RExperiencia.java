e00f6d7bb5508667949c5a6ed7d5f6ebfc207d08
Tomás Repetto <tomyrep04@gmail.com>
2023-04-09 16:44:43 -0300

    control experiencia

---
+++ BackEnd/src/main/java/com/portfolio/portfolio/Repository/RExperiencia.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.portfolio.Repository;
+
+import com.portfolio.portfolio.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author tomyr
+ */
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer> {
+    public Optional<Experiencia> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
