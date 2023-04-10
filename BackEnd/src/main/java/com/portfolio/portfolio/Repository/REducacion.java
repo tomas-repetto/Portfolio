445b371f971a9edb77d1d95d22a2298383736d83
Tomás Repetto <tomyrep04@gmail.com>
2023-04-09 22:07:14 -0300

    backend educacion

---
+++ BackEnd/src/main/java/com/portfolio/portfolio/Repository/REducacion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.portfolio.Repository;
+
+import com.portfolio.portfolio.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author tomyr
+ */
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
---
+++ BackEnd/src/main/java/com/portfolio/portfolio/Service/SEducacion.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.portfolio.Service;
+
+import com.portfolio.portfolio.Entity.Educacion;
+import com.portfolio.portfolio.Repository.REducacion;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author tomyr
+ */
+@Service
+@Transactional
+public class SEducacion {
+    @Autowired
+    REducacion rEducacion;
+    
+    public List<Educacion> list(){
+        return rEducacion.findAll();
+    }
+    
+    public Optional<Educacion> getOne(int id){
+        return rEducacion.findById(id);
+    }
+    
+    public Optional<Educacion> getByNombreE(String nombreE){
+        return rEducacion.findByNombreE(nombreE);
+    }
+    
+    public void save(Educacion educacion){
+        rEducacion.save(educacion);
+    }
+    
+    public void delete(int id){
+        rEducacion.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rEducacion.existsById(id);
+    }
+    
+    public boolean existsByNombreE(String nombreE){
+        return rEducacion.existsByNombreE(nombreE);
+    }
+    
+    
+}
